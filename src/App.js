3de285b78fc634ea53b24c308a3433f70969fcca
am0r90 <thswlsdud909@gmail.com>
2020-02-02 14:09:59 +0900

    https://github.com/am0r90/movie_app_2020/

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Potato from "./Potato";
+
 
 function App() {
     return (
         <div>
             <h1>GHello!!!!!!!!</h1>
+            <Potato />
         </div>
         );
 }
